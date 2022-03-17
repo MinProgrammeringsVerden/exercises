2707b7f261c0ff125176e296da1b25e8f96b6134
MonikaPawlowska <45140275+MinProgrammeringsVerden@users.noreply.github.com>
2022-03-17 23:24:05 +0100

    Update List.js

--- src/component/List.js
+++ src/component/List.js
@@ -4,7 +4,7 @@ function List(props) {
     return (
         <div> 
            <h1> List here !!! </h1>
-           <h2> Some changes inserted directly frm github account itself </h2>
+           <h2> Some changes inserted directly from github account itself  !!! </h2>
         
         </div>
     );
