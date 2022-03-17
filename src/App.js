5b0a792ab27acc79fed79eb298956d62d39bb4ce
MinProgrammeringsVerden <my.programming.world.pl@gmail.com>
2022-03-17 23:07:58 +0100

    modified app file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Exercises App
+      Exercises App, new dev branch is here 
+      main is also here 
     </div>
   );
 }
